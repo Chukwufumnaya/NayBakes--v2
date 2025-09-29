42ee0cb20e8355074d6f3e2ba73b163621b4b918
Chukwufumnaya <naomiokelue2002@gmail.com>
2025-09-29 10:13:33 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-red-600 underline text-3xl">Hello world</h1>
+      <p className="text-yellow-600 font-semibold">Naomi</p>
+    </>
+
+  )
+}
