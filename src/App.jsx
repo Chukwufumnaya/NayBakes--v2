80c6c99f343ff8342620aa37ba6acad5d952937b
Chukwufumnaya <naomiokelue2002@gmail.com>
2025-10-10 19:20:44 +0100

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./components/CartContext";
 import MainPage from "./pages/MainPage";
 import MenuPage from "./pages/MenuPage";
 import Cart from "./pages/Cart";
-import { CartProvider } from "./components/CartContext";
 import About from "./pages/AboutUs";
 
 export default function App() {
   return (
-    <CartProvider>
-      <Router >
+    <HashRouter >
+      <CartProvider>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/about" element={<About />} />
         </Routes>
-      </Router>
-    </CartProvider>
+      </CartProvider>
+    </HashRouter>
   );
 }
