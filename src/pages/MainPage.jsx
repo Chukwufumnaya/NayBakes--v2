915127a8a954022e000dc76e2641652fabe72acb
Chukwufumnaya <naomiokelue2002@gmail.com>
2025-10-26 20:50:47 +0100

    Implemented carousel features

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,6 +6,9 @@ import { TypeAnimation } from "react-type-animation";
 import { info } from "../data/data";
 import { reviews } from "../data/reviews";
 import { FaInstagram, FaFacebook, FaXTwitter, FaHandPointUp } from "react-icons/fa6";
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function FlippableCard({ item }) {
   const [flipped, setFlipped] = useState(false);
@@ -18,8 +21,9 @@ function FlippableCard({ item }) {
   };
 
   return (
+
     <div
-      className={`bg-[#D5C4A1] p-8 py-5 w-[270px] h-[280px] cursor-pointer [perspective:1000px] group rounded-xl shadow`}
+      className={`bg-[#D5C4A1] p-8 py-5 w-[270px] h-[280px] cursor-pointer [perspective:1000px] group rounded-xl shadow m-auto`}
       onClick={handleFlip}
     >
       {isMobile && !flipped && (
@@ -69,16 +73,55 @@ export default function MainPage() {
   ))
 
   const customerReviews = reviews.map(review => (
-    <div key={review.id} className="bg-[#D5C4A1] p-5 w-[250px] h-[230px] rounded-xl flex flex-col justify-center items-center">
+    <div key={review.id} className="p-5 w-[250px] h-[230px] flex flex-col justify-center items-center text-xs">
       <img
         src={review.avatar}
         alt="avatar"
         loading="lazy"
-        className="w-[80px] m-4" />
+        className="w-[50px] m-auto" />
       <p>&quot; {review.review} &quot;</p>
     </div>
   ))
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 0,
+          dots: true
+        }
+      }
+    ]
+  };
 
   return (
     <>
@@ -111,15 +154,17 @@ export default function MainPage() {
         </section>
         <section className="mt-6 text-center">
           <HashLink to="/menu#menu">
-            <button className="bg-[#D5C4A1] p-3 rounded-3xl text-sm sm:text-lg cursor-pointer hover:bg-[#9b8e73]">View our menu and place your order now!</button>
+            <button className="bg-[#D5C4A1] p-3 rounded-3xl text-sm sm:text-lg cursor-pointer hover:bg-[#9b8e73]">Click to view our menu and place your order.</button>
           </HashLink>
         </section>
         {/** Popular Items Section*/}
         <section className="flex flex-col items-center justify-center mt-6 text-center">
           <h2 className="mb-4 text-4xl font-normal">POPULAR ITEMS</h2>
           <p>These are popular with our customers.</p>
-          <div className="grid items-center grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 mt-7 place-items-center mb-7">
-            {popularCardContent}
+          <div className="w-[90%] mx-auto p-4 md:p-8">
+            <Slider {...settings}>
+              {popularCardContent}
+            </Slider>
           </div>
         </section>
 
@@ -127,8 +172,10 @@ export default function MainPage() {
         <section className="flex flex-col items-center justify-center mt-6 text-center">
           <h2 className="mt-6 mb-4 text-4xl font-normal">TOP PRODUCTS</h2>
           <p>These are our most sold out products this week.</p>
-          <div className="grid items-center grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 mt-7 place-items-center">
-            {topCardContent}
+          <div className="w-[90%] mx-auto p-4 md:p-8">
+            <Slider {...settings}>
+              {topCardContent}
+            </Slider>
           </div>
         </section>
 
@@ -136,8 +183,10 @@ export default function MainPage() {
         <section className="flex flex-col items-center justify-center mt-6 text-center" id="reviews">
           <h2 className="mt-6 mb-4 text-4xl font-normal">REVIEWS</h2>
           <p>Here&apos;s what some of our customers have to say</p>
-          <div className="grid items-center grid-cols-1 gap-10 text-xs bg-center sm:grid-cols-2 lg:grid-cols-3 mt-7 place-items">
-            {customerReviews}
+          <div className="w-[90%] mx-auto p-4 md:p-8">
+            <Slider {...settings}>
+              {customerReviews}
+            </Slider>
           </div>
         </section>
         <div className="border-2 border-[#d5c4a1b4] w-11/12 m-auto mt-7"></div>
